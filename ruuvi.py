8023ac571f34d0366ca1194705659f525c0a5b1f
Albert Offei <abodi3@Alberts-MacBook-Air-1958.local>
2019-11-06 07:43:49 +0200

    update to ruuvi, now sending data to influxdb

--- ruuvi.py
+++ ruuvi.py
@@ -1,7 +1,32 @@
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from influxdb import InfluxDBClient
 
-def handle_data(found_data):
-    print('MAC ' + found_data[0])
-    print(found_data[1])
 
-RuuviTagSensor.get_datas(handle_data)
+def main(found_data):
+    #print('MAC ' + found_data[0])
+    #print(found_data[1])
+
+    temp = found_data[1].get("temperature")
+    humidity = found_data[1].get('humidity')
+
+    datapoints =  [
+    {
+        "measurement": "ruuvi_metrics",
+        "tags": {
+            "host": "Obibini",
+            "region": "Fi Vantaa"
+        },
+
+        "fields": {
+            "temperature": temp 
+            "humidity": humidity
+
+        }
+    }
+	]
+
+   
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'ofienipa')
+    client.write_points(datapoints)
+
+RuuviTagSensor.get_datas(main)
