1c8aef9e6ee897729a27a079b31a0c888e24dd88
Anh Nguyen <33486007+anhnnt@users.noreply.github.com>
2019-11-25 23:02:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 import unittest
-import app
+import ruuvi
 
 class TestHello(unittest.TestCase):
 
     def setUp(self):
-        app.app.testing = True
-        self.app = app.app.test_client()
+        ruuvi.ruuvi.testing = True
+        self.ruuvi = ruuvi.ruuvi.test_client()
 
     def test_hello(self):
-        rv = self.app.get('/')
+        rv = self.ruuvi.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_hello(self):
-        rv = self.app.get('/hello/')
+        rv = self.ruuvi.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
